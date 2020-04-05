688af94985542104f61fa4f81c40ddefbccaec1b
mprzybolewski <mprzybolewski@gmail.com>
2020-04-05 10:37:45 +0200

    add nwd

--- multicontapp/index.js
+++ multicontapp/index.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const redis = require("redis");
+const express = require('express');
+const redis = require('redis');
+const process = require('process');
 
 const app = express();
 
@@ -8,8 +9,32 @@ const client = redis.createClient({
   port: 6379
 });
 
-app.get('/', (req, res) => {
-  res.send("Hello from my docker webbbb app");
+client.set('counter', 0);
+
+app.get('/', (req, resp) => {
+  client.get('counter', (err, counter_value) => {
+    resp.send('Counter: ' + counter_value);
+    client.set('counter', parseInt(counter_value) + 1);
+  });
+});
+
+app.get("/nwd", (req, res) => {
+  const l1 = req.query.l1;
+  const l2 = req.query.l2;
+
+  const key = `${l1}_${l2}`;  
+  client.exists(key, (err, exists) => {
+    if (exists === 1) {
+      client.get(key, (err, nwd) => {
+        res.send(nwd);
+        return;
+      });
+    } else {
+      const nwd = countNWD(l1, l2);
+      client.set(key, nwd);
+      res.send(nwd);
+    }
+  });
 });
 
 app.listen(8090, () => {
